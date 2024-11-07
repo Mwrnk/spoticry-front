3f87463d9f3e55873622f7109dda50a50da133c6
Werneck <mateuswerneckelizeu@yahoo.com.br>
2024-11-07 18:43:18 -0300

    style: adicionando um spinner na pagina discover

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "scale-up4": "scale-up4 1s linear infinite",
+      },
+      keyframes: {
+        "scale-up4": {
+          "20%": { transform: "scaleY(1.5)", backgroundColor: "#fff" },
+          "40%": { transform: "scaleY(1)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
