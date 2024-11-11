74a9654e88820cfaf195ecb92e397f353f1f0abe
Tiago Malaquias <tiago.malaquias.souza@gmail.com>
2024-11-11 09:13:29 -0300

    feat: criar página Musics
    Para listagem e adição de músicas pelo usuário.

--- src/pages/Musics.jsx
+++ src/pages/Musics.jsx
@@ -1,7 +1,57 @@
+import { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import SearchBar from "../components/SearchBar";
+import SearchResults from "../components/SearchResults";
+import { fetchSongs } from "../services/api";
+import SongsList from "../components/SongsList";
+
 function Musics() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [songs, setSongs] = useState([]);
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  const clearSearch = () => {
+    setSearchQuery("");
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchSongs();
+      setSongs(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>Musics</h1>
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      <Header />
+      <main className="flex overflow-hidden ">
+        <Sidebar />
+
+        <div className="flex-1 overflow-y-auto p-4">
+          <SearchBar
+            searchQuery={searchQuery}
+            placeholder="Pesquisar em suas músicas..."
+            onSearch={handleSearch}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <h1 className="text-2xl font-bold">Suas musicas</h1>
+          {searchQuery ? (
+            <SearchResults
+              searchQuery={searchQuery}
+              songs={songs}
+              onClearSearch={clearSearch}
+            />
+          ) : (
+            <SongsList songs={songs} />
+          )}
+        </div>
+      </main>
     </div>
   );
 }
