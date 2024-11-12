1eb749d53b2581335ae01a07271721fe8eb4333a
Mateus Werneck <mateuswerneckelizeu@yahoo.com.br>
2024-11-12 14:14:16 -0300

    feat: adicionando botões de edição, caso o usuário esteja na pagina Musics

--- src/components/SongsList.jsx
+++ src/components/SongsList.jsx
@@ -1,11 +1,39 @@
-function SongsList({ songs }) {
+// src/components/SongsList.jsx
+function SongsList({ songs, canEditfea }) {
   return (
     <div className="grid grid-cols-1 gap-4">
       {songs.map((song) => (
-        <div key={song.id} className="p-4 bg-gray-700 rounded-lg shadow-md">
+        <div
+          key={song.id}
+          className="flex flex-col p-4 bg-gray-700 rounded-lg shadow-md"
+        >
           <h3 className="text-lg text-white">{song.title}</h3>
           <p className="text-gray-400">{song.artist}</p>
           <p className="text-gray-500">{song.userId}</p>
+          <div className="flex flex-row mt-4 border-t border-gray-600">
+            <button
+              onClick={() => playSong(song.url)}
+              className="flex items-center p-4 m-4 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
+            >
+              Play
+            </button>
+            {canEdit && (
+              <>
+                <button
+                  onClick={() => deleteSong(song.id)}
+                  className="flex items-center p-4 m-4 text-white bg-red-500 rounded-lg hover:bg-red-600"
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={() => editSong(song.id)}
+                  className="flex items-center p-4 m-4 text-white bg-green-500 rounded-lg hover:bg-green-600"
+                >
+                  Edit
+                </button>
+              </>
+            )}
+          </div>
         </div>
       ))}
     </div>
