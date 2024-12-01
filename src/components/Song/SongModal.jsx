9012f63d2e4cab22c0d36dd3776c3d4a6d18f5e2
Pedro Marazo <pedromarazo@outlook.com>
2024-12-01 20:46:44 -0300

    style: adicionando estilo para o modal de adição/edição de músicas

--- src/components/Song/SongModal.jsx
+++ src/components/Song/SongModal.jsx
@@ -14,9 +14,9 @@ function SongModal({ isOpen, onClose, onSave, music = {} }) {
   if (!isOpen) return null; // Não renderiza o modal se não estiver aberto
 
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
-      <div className="bg-gray-600 p-6 rounded-lg shadow-lg w-80">
-        <h2 className="text-lg font-semibold mb-4">
+    <div className="fixed inset-0 bg-zinc-900 bg-opacity-75 flex items-center justify-center drop-shadow-2xl">
+      <div className="bg-zinc-800 p-6 rounded-lg shadow-lg w-80">
+        <h2 className="text-2xl font-semibold mb-4">
           {isEditing ? "Editar Música" : "Adicionar Música"}
         </h2>
         <input
@@ -24,14 +24,14 @@ function SongModal({ isOpen, onClose, onSave, music = {} }) {
           placeholder="Título"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          className="w-full p-2 mb-2 border rounded"
+          className="w-full p-2 mb-2  rounded-lg bg-zinc-700"
         />
         <input
           type="text"
           placeholder="Artista"
           value={artist}
           onChange={(e) => setArtist(e.target.value)}
-          className="w-full p-2 mb-2 border rounded"
+          className="w-full p-2 mb-2  rounded-lg bg-zinc-700"
         />
         {!isEditing && (
           <input
@@ -39,19 +39,19 @@ function SongModal({ isOpen, onClose, onSave, music = {} }) {
             placeholder="URL"
             value={url}
             onChange={(e) => setUrl(e.target.value)}
-            className="w-full p-2 mb-4 border rounded"
+            className="w-full p-2 mb-4  rounded-lg bg-zinc-700"
           />
         )}
         <div className="flex justify-end">
           <button
             onClick={onClose}
-            className="px-4 py-2 bg-gray-300 text-gray-700 rounded mr-2"
+            className="px-4 py-2 bg-gray-300 text-zinc-700 rounded mr-2 hover:bg-gray-400 transition-transform transform hover:scale-105"
           >
             Cancelar
           </button>
           <button
             onClick={handleSave}
-            className="px-4 py-2 bg-blue-500 text-white rounded"
+            className="px-4 py-2 bg-gradient-to-r from-custom-blue to-custom-purple  text-white font-semibold rounded transition-transform transform hover:scale-105"
           >
             {isEditing ? "Salvar Alterações" : "Adicionar"}
           </button>
